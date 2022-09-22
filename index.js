4d40c6e129fb334542e1af42f0884679612d82ce
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2022-09-22 19:16:54 +0200

    Debug

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ async function run() {
 }
 
 function toNudge(inputArgs, context){
+  console.log(`Input args: ${JSON.stringify(inputArgs)}`);
   let conclussions = util.getArrayFromString(inputArgs.conclussions);
   return conclussions.includes(context.conclussion);
 }
